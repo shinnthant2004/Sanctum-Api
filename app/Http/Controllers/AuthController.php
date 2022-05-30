078b87bcdbf38a00e6489265255c93a92fea796b
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-05-30 23:26:17 +0630

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -21,5 +22,17 @@ class AuthController extends Controller
             'message' => 'successfully Registered',
             'token' => $token->plainTextToken
         ]);
+    }
+    public function login()
+    {
+        if (Auth::attempt(['email' => request()->email, 'password' => request()->password])) {
+            $user = auth()->user();
+            $token = $user->createToken('testing');
+            return response()->json([
+                'status' => 200,
+                'message' => 'successfully Registered',
+                'token' => $token->plainTextToken
+            ]);
+        }
     }
 }
