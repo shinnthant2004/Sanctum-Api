c33a0fe8ee9e66d45ba099ede60cac8ceb4f90b5
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-05-30 23:05:42 +0630

    register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register()
     {
-        return "A";
+        $user = new User();
+        $user->name = request()->name;
+        $user->email = request()->email;
+        $user->password = Hash::make(request()->password);
+        $user->save();
+        $token = $user->createToken('testing');
+        return response()->json([
+            'status' => 200,
+            'message' => 'successfully Registered',
+            'token' => $token->plainTextToken
+        ]);
     }
 }
